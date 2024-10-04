7fd31da6eec1404232636bdc0fad2df7464970f0
vikasmadaan <vikasmadaang@gmail.com>
2024-10-04 17:01:45 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+m = st.markdown("""
+<style>
+.counter-text {
+        font-size: 24px;
+        color: black;
+        font-weight: bold;
+        margin: auto;
+        display: block;
+                text-align:center;
+    }
+div.stButton > button:first-child {
+    background-color: #007bff;;
+    color: white;
+    height: 3em;
+    width: 12em;
+    border-radius:10px;
+    border:3px solid #000000;
+    font-size:20px;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+}
+</style>""", unsafe_allow_html=True)
+
+
+
+# Initialize the counter in session state if it doesn't exist
+st.session_state.counter = st.session_state.get('counter', 0)
+
+
+# Button to increment the counter
+if st.button("Click me!"):
+    st.session_state.counter += 1
+
+st.markdown(f'<div class="counter-text">Counter:{st.session_state.counter}</div>', unsafe_allow_html=True)
